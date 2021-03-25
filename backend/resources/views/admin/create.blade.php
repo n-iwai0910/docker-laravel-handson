100d167fcbcb08ed261a821327c1febf09723904
iwai <n.iwai@e-ize.jp>
2021-03-25 16:06:52 +0900

    フォーム修正

--- backend/resources/views/admin/create.blade.php
+++ backend/resources/views/admin/create.blade.php
@@ -34,62 +34,32 @@
 
                     <div id="drop-zone" style="border: 1px solid; padding: 30px;">
                         <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image2" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview2">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
+                        <input type="file" class="form-control" id="image" name="files[][image]" accept="image/*"><br>
+                        <input type="file" class="form-control" id="image2" name="files[][image]" accept="image/*"><br>
                         <input type="file" class="form-control" id="image3" name="files[][image]" accept="image/*">
                     </div>
+                    <img id="preview">
+                    <img id="preview2">
                     <img id="preview3">
-
                     <div id="drop-zone" style="border: 1px solid; padding: 30px;">
                         <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image4" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview4">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image5" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview5">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
+                        <input type="file" class="form-control" id="image4" name="files[][image]" accept="image/*"><br>
+                        <input type="file" class="form-control" id="image5" name="files[][image]" accept="image/*"><br>
                         <input type="file" class="form-control" id="image6" name="files[][image]" accept="image/*">
                     </div>
+                    <img id="preview4">
+                    <img id="preview5">
                     <img id="preview6">
-
                     <div id="drop-zone" style="border: 1px solid; padding: 30px;">
                         <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image7" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview7">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image8" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview8">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
-                        <input type="file" class="form-control" id="image9" name="files[][image]" accept="image/*">
-                    </div>
-                    <img id="preview9">
-
-                    <div id="drop-zone" style="border: 1px solid; padding: 30px;">
-                        <p>ファイルをドラッグ＆ドロップもしくは</p>
+                        <input type="file" class="form-control" id="image7" name="files[][image]" accept="image/*"><br>
+                        <input type="file" class="form-control" id="image8" name="files[][image]" accept="image/*"><br>
+                        <input type="file" class="form-control" id="image9" name="files[][image]" accept="image/*"><br>
                         <input type="file" class="form-control" id="image10" name="files[][image]" accept="image/*">
                     </div>
+                    <img id="preview7">
+                    <img id="preview8">
+                    <img id="preview9">
                     <img id="preview10">
                     <br>
                     <hr>
